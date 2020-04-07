9bb94470d4e9d0203c7f65cbe655d5aa4dd68852
CINCONUMBERCERO <cinconumbercero@hotmail.com>
2020-04-07 00:13:08 -0500

    Community fragment ready for action

--- app/src/main/java/com/example/habitapp/ui/Community/CommunityViewModel.kt
+++ app/src/main/java/com/example/habitapp/ui/Community/CommunityViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.habitapp.ui.dashboard
+package com.example.habitapp.ui.Community
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class CommunityViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "Our Habits"
